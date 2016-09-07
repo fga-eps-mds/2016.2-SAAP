a449da29fbfdd37e814ceb0d67a5426a173f46f4
Vinicius Oliveira <vinifladf@gmail.com>
2016-09-07 00:22:08 -0300

    Arrumando rotas

--- saap/autenticacao/urls.py
+++ saap/autenticacao/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from .views import (LoginView)
 
 urlpatterns = [
+    url(r'^$', LoginView.as_view(),
+        name='login'),
     url(r'^login/$', LoginView.as_view(),
         name='login'),
 ]
-
