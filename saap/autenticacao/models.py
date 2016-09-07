a8edcb097fe167ba2826947cb9a8508ea2575920
Vinicius Oliveira <vinifladf@gmail.com>
2016-09-07 00:22:08 -0300

    Arrumando rotas

--- saap/autenticacao/models.py
+++ saap/autenticacao/models.py
@@ -9,3 +9,11 @@ class Usuario_saap(User):
     data_de_nascimento = models.DateField()
     sexo = models.CharField(max_length=250)
     municipio = models.CharField(max_length=250)
+
+    @classmethod
+    def buscaNome(cls, nome):
+        return Usuario_saap.objects.filter(first_name__startswith(nome))
+
+    @classmethod
+    def deletaUsuario(cls, idArg):
+        Usuario_saap.objects.get(id=idArg).delete()
