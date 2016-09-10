95d3b321d2808c75911fb94ccd6c31d92de08cb8
Mateus de Morais <mateusmorais@hotmail.com.br>
2016-09-10 17:52:57 -0300

    Metodos de classe para filtrar usuarios
    Signed-off-by: Mateus de Morais <mateusmorais@hotmail.com.br>
    Signed-off-by: Renata Souza <renata.rfsouza@gmail.com>
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
    Signed-off-by: Vinicius Rodrigues Oliveira <vinifladf@gmail.com>

--- saap/autenticacao/models.py
+++ saap/autenticacao/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import *
 
 
 # Create your models here.
@@ -11,9 +12,17 @@ class Usuario_saap(User):
     municipio = models.CharField(max_length=250)
 
     @classmethod
-    def buscaNome(cls, nome):
+    def busca_nome(cls, nome):
         return Usuario_saap.objects.filter(first_name__startswith(nome))
 
     @classmethod
-    def deletaUsuario(cls, idArg):
+    def deleta_usuario(cls, idArg):
         Usuario_saap.objects.get(id=idArg).delete()
+
+    @classmethod
+    def busca_username(cls, username):
+        return Usuario_saap.objects.filter(username__startswith(username))
+
+    @classmethod
+    def get_usuario_por_username(cls, username):
+        return Usuario_saap.objects.filter(username = username)
--- saap/autenticacao/urls.py
+++ saap/autenticacao/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import (LoginView)
+from .views import (LoginView, RegistroView)
 
 urlpatterns = [
     url(r'^$', LoginView.as_view(),
         name='login'),
     url(r'^login/$', LoginView.as_view(),
         name='login'),
+    url(r'^cadastro/$', RegistroView.as_view(),
+        name='cadastro'),
 ]
