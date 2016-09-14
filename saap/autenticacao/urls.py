94844c7f746a879405fbae160b440770da35a2ea
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2016-09-13 23:28:26 -0300

    Recuperação de senha

--- saap/autenticacao/urls.py
+++ saap/autenticacao/urls.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 from django.conf.urls import url
 from .views import (LoginView, RegistroView)
+"""from django.contrib import admin"""
+"""from utils.views import ResetPasswordRequestView, PasswordResetConfirmView"""
+"""from django.conf.urls import include, url"""
 
 
 urlpatterns = [
@@ -10,4 +13,10 @@ urlpatterns = [
         name='login'),
     url(r'^cadastro/$', RegistroView.as_view(),
         name='cadastro'),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^recuperacao_senha(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    #    PasswordResetConfirmView.as_view(), name='reset_password_confirm'),
+    #url(r'^recuperacao_senha',
+    #    ResetPasswordRequestView.as_view(), name="recuperacao_senha")"""
+
 ]
