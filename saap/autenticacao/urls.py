cd4febf9494e8e5281a102195f7c7ad5cfdbf794
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2016-09-17 19:53:06 -0300

    Configurando a url para a funcionalidade Mudar senha

--- saap/autenticacao/urls.py
+++ saap/autenticacao/urls.py
@@ -1,10 +1,7 @@
 # coding=utf-8
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from .views import (LoginView, RegistroView, PerfilView, LogoutView)
-"""from django.contrib import admin"""
-"""from utils.views import ResetPasswordRequestView, PasswordResetConfirmView"""
-"""from django.conf.urls import include, url"""
+from .views import (LoginView, RegistroView, PerfilView, LogoutView, MudarSenhaView)
 
 
 urlpatterns = [
@@ -18,10 +15,7 @@ urlpatterns = [
         name='perfil'),
     url(r'^logout/$', login_required(LogoutView.as_view()),
         name='logout'),
-    #url(r'^admin/', include(admin.site.urls)),
-    #url(r'^recuperacao_senha(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    #    PasswordResetConfirmView.as_view(), name='reset_password_confirm'),
-    #url(r'^recuperacao_senha',
-    #    ResetPasswordRequestView.as_view(), name="recuperacao_senha")"""
+    url(r'^mudar_senha/$', login_required(MudarSenhaView.as_view()),
+        name='mudar_senha'),
 
 ]
