9c0a971c8e33537f6b407c81a9620c1adf5845ab
Renatafsouza <renata.rfsouza@gmail.com>
2016-10-15 14:12:33 -0300

    add imports em model autenticacao
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>
    Signed-off-by: Renatafsouza <renata.rfsouza@gmail.com>

--- saap/autenticacao/models.py
+++ saap/autenticacao/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import *
-from core.models import Contato, Ticket
+from core.models import Contato, Ticket, Documento, Boletim, Template
 
 
 # Create your models here.
