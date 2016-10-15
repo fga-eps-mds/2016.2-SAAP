c5573baca31d2c979efefc502e4957d711c40a6d
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-10-15 16:15:19 -0300

    Teste de delete ticket view

--- saap/core/tests/test_views.py
+++ saap/core/tests/test_views.py
@@ -7,7 +7,7 @@ from autenticacao.models import OrganizadorContatos
 
 
 @pytest.mark.django_db
-def test_deletar_ticket():
+def test_enviar_ticket():
     client = Client()
 
     organizador = OrganizadorContatos()
@@ -38,4 +38,45 @@ def test_deletar_ticket():
     response = client.post('/ticket/',request)
     tickets = Ticket.objects.all().count()
 
-    assert tickets is 1
+    assert tickets == 1
+    Ticket.objects.all()[0].delete()
+
+
+
+@pytest.mark.django_db
+def test_deletar_ticket():
+    client = Client()
+
+    organizador = OrganizadorContatos()
+    
+    organizador.username = 'sabino'
+    organizador.first_name = 'sabino'
+    organizador.data_de_nascimento = '1990-01-01'
+    organizador.sexo = 'masculino'
+    organizador.municipio = 'ceilandia'
+    organizador.uf = 'df'
+    organizador.set_password('eusoueu0')
+
+    organizador.save()
+
+    client.login(username='sabino', password='eusoueu0')
+
+    request = {
+    "nome_organizador":"sabino",
+    "enviar_anonimamente":"False",
+    "assunto" : "blablabla",
+    "descricao" : "corpo_texto",
+    "envio_anonimo": "anonimo",
+    "tipo_mensagem": "blablabla"
+    }
+
+    client.post('/ticket/',request)
+
+    tickets_before = Ticket.objects.all().count()
+    
+    ticket = Ticket.objects.all().last()
+
+    client.get('/deletar_ticket/'+str(ticket.id),follow=True)
+    
+    tickets_after = Ticket.objects.all().count()
+    assert tickets_before > tickets_after
