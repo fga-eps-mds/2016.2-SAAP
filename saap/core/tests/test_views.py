a60734d426b25acb3dff05dac3b1052fac194e8b
Mateus de Morais <mateusmorais@hotmail.com.br>
2016-10-22 11:13:53 -0200

    Testes view enviar carta POST

--- saap/core/tests/test_views.py
+++ saap/core/tests/test_views.py
@@ -12,7 +12,7 @@ def test_enviar_ticket():
     client = Client()
 
     organizador = OrganizadorContatos()
-    
+
     organizador.username = 'sabino'
     organizador.first_name = 'sabino'
     organizador.data_de_nascimento = '1990-01-01'
@@ -48,7 +48,7 @@ def test_deletar_ticket():
     client = Client()
 
     organizador = OrganizadorContatos()
-    
+
     organizador.username = 'sabino'
     organizador.first_name = 'sabino'
     organizador.data_de_nascimento = '1990-01-01'
@@ -73,11 +73,11 @@ def test_deletar_ticket():
     client.post('/ticket/',request)
 
     tickets_before = Ticket.objects.all().count()
-    
+
     ticket = Ticket.objects.all().last()
 
     client.get('/deletar_ticket/'+str(ticket.id),follow=True)
-    
+
     tickets_after = Ticket.objects.all().count()
     assert tickets_before > tickets_after
 
@@ -106,3 +106,63 @@ def test_vereadores_view_post_existe():
 	response = client.post('/vereadores/', {'nome_organizador': 'Organizador'})
 	assert response.status_code is 200
 	organizador.delete()
+
+@pytest.mark.django_db
+def test_enviar_carta_view_get_organizador_logado():
+
+    organizador = OrganizadorContatos()
+    organizador.username = 'orgteste'
+    organizador.set_password('123')
+    organizador.data_de_nascimento = '1900-01-01'
+    organizador.save()
+    client = Client()
+    client.post('/', {'username': 'orgteste', 'password': '123'})
+    response = client.get('/enviar_carta/')
+    assert response.status_code is 200
+    organizador.delete()
+
+@pytest.mark.django_db
+def test_enviar_carta_view_get_organizador_deslogado():
+
+    cidadao = Cidadao()
+    cidadao.username = 'cidteste'
+    cidadao.set_password('123')
+    cidadao.data_de_nascimento = '1900-01-01'
+    cidadao.save()
+    client = Client()
+    client.post('/', {'username': 'cidteste', 'password': '123'})
+    response = client.get('/enviar_carta/')
+    assert 300 <= response.status_code < 400
+    cidadao.delete()
+
+@pytest.mark.django_db
+def test_enviar_carta_view_post():
+
+    organizador = OrganizadorContatos()
+    organizador.username = 'orgteste'
+    organizador.set_password('123')
+    organizador.data_de_nascimento = '1900-01-01'
+    organizador.save()
+    client = Client()
+    client.post('/', {'username': 'orgteste', 'password': '123'})
+    response = client.post('/enviar_carta/', {'nome_remetente': 'Remetente', \
+        'municipio_remetente': 'Município', 'nome_destinatario': 'Destinatário'\
+        , 'forma_tratamento': 'Senhor(a)', 'mensagem': 'Mensagem'})
+    assert response.status_code is 200
+    organizador.delete()
+
+@pytest.mark.django_db
+def test_enviar_carta_view_post_faltando_campo():
+
+    organizador = OrganizadorContatos()
+    organizador.username = 'orgteste'
+    organizador.set_password('123')
+    organizador.data_de_nascimento = '1900-01-01'
+    organizador.save()
+    client = Client()
+    client.post('/', {'username': 'orgteste', 'password': '123'})
+    response = client.post('/enviar_carta/', {'nome_remetente': 'Remetente', \
+        'municipio_remetente': 'Município', 'nome_destinatario': 'Destinatário'\
+        , 'forma_tratamento': 'Senhor(a)', 'mensagem': ''})
+    assert response.status_code is 200
+    organizador.delete()
