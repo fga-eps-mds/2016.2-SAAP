b0e17f63138e3c88904a84a9fc44132e789dc2c1
Renatafsouza <renata.rfsouza@gmail.com>
2016-10-24 13:33:13 -0200

    Metodos queryset de GrupoDeContato

--- saap/core/views.py
+++ saap/core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.template import RequestContext
 from django.utils.translation import ugettext
 from core.models import Contato, Ticket
+from autenticacao.models import OrganizadorContatos
 from default.views import *
 from autenticacao.views import *
 from autenticacao.models import *
@@ -303,7 +304,6 @@ POST['nome_organizador'])
 
         return resposta
 
-
 class GerarCartaView(View):
     http_method_names = [u'get', u'post']
 
@@ -393,7 +393,6 @@ class EnviarCartaView(View):
         return enviar_carta_email(request, carta)
 
 
-
 class BuscaContatosView(ListView):
     http_method_names = [u'post']
 
@@ -461,4 +460,29 @@ class AdicionarContatoAoGrupo(View):
 
 class GrupoDeContatos(ListView):
     http_method_names = [u'get', u'post']
+
+=======
+class GrupoDeContatosView(ListView):
+    http_method_names = [u'get', u'post']
+
+    model = Contato #grupo
+    template_name = 'grupo_de_contatos.html'
+
+    def getGrupo (self, **kwargs):
+        return Contato.objects(grupo)
+
+    def getData (self, **kwargs):
+        return Contato.objects.filter(data_de_nascimento)
+
+    def getBairro (self, **kwargs):
+        return Contato.objects.filter(bairro)
+
+    def getCidade(self, **kwargs):
+        return Contato.objects.filter(cidade)
+
+    def getCEP(self, **kwargs):
+        return Contato.objects.filter(CEP)
+
+    def getUF (self, **kwargs):
+        return Contato.objects.filter(UF)
 
