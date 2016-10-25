83369fab03788b0ee96e61b9e773b1e38b80e583
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-10-25 15:50:47 -0200

    Atualizando metodo de Busca e template
    Signed-off-by: Renatafsouza <renata.rfsouza@gmail.com>
    Signed-off-by: Gustavo Sabino <gustavo.sabino@hotmail.com>

--- saap/core/views.py
+++ saap/core/views.py
@@ -689,7 +689,7 @@ class BuscaContatosView(ListView):
         if busca == 'cidade':
             resposta = Contato.objects.filter(cidade__startswith=query)
         elif busca == 'genero':
-            resposta = Contato.objects.filter(sexo__contais=query)
+            resposta = Contato.objects.filter(sexo__contains=query)
         elif busca == 'estado':
             resposta = Contato.objects.filter(estado__startswith=query)
         elif busca == 'data_aniversario':
