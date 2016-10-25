e70a4e3d93e723d5db49e96e49917e3e396f7094
Vinicius Oliveira <vinifladf@gmail.com>
2016-10-25 15:42:00 -0200

    Corrigindo erros dentro da branch
    Signed-off-by: Vinicius Oliveira <vinifladf@gmail.com>
    Signed-off-by: Anna Larissa <annalarissa.martins@hotmail.com>

--- saap/autenticacao/models.py
+++ saap/autenticacao/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import *
-from core.models import Contato, Ticket, Oficio, Boletim, Template, Carta
+from core.models import *
 
 
 # Create your models here.
