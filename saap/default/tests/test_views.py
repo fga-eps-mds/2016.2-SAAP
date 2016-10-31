3154f7bae8438900aa73f5d35d71484792af8d80
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2016-10-31 14:17:59 -0200

    Teste do método checar_vazio()

--- saap/default/tests/test_views.py
+++ saap/default/tests/test_views.py
@@ -7,3 +7,7 @@ def test_checar_campos():
     campos = ['teste']
     response = checar_campos(campos)
     assert response is True
+
+def test_checar_vazio():
+	campos = ['']
+	assert checar_vazio(campos) is False
