e57644a7f2adb316f56507765dfe64788bc2a024
Vinicius Oliveira <vinifladf@gmail.com>
2016-11-05 17:58:34 -0200

    Arrumando erro de return

--- saap/default/views.py
+++ saap/default/views.py
@@ -190,6 +190,52 @@ def gerar_pdf_carta(carta):
     mensagem = carta.texto
     mensagem = mensagem.replace('\n', '<br/>')
 
+    Story=[]
+
+    now = datetime.now()
+
+    styles=getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+    ptext = '<font size=12>%s, %s/%s/%s</font>' % (carta.municipio_remetente, now.day, now.month, now.year)
+    Story.append(Paragraph(ptext, styles["Normal"]))
+
+    Story.append(Spacer(1, 24))
+
+    ptext = '<font size=12>%s %s,</font>' % (carta.forma_tratamento, carta.nome_destinatario)
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    #.split()[0].strip()
+
+    Story.append(Spacer(1, 36))
+
+    ptext = '<font size=12>Prezado %s:</font>' % carta.forma_tratamento
+    Story.append(Paragraph(ptext, styles["Normal"]))
+
+    Story.append(Spacer(1, 12))
+
+    ptext = '<font size=12>%s</font>' % mensagem
+    Story.append(Paragraph(ptext, styles["Justify"]))
+
+    Story.append(Spacer(1, 36))
+
+    ptext = '<font size=12>Atenciosamente,</font>'
+    Story.append(Paragraph(ptext, styles["Normal"]))
+
+    Story.append(Spacer(1, 12))
+
+    ptext = '<font size=12>%s</font>' % carta.nome_remetente
+    Story.append(Paragraph(ptext, styles["Normal"]))
+
+    Story.append(Spacer(1, 12))
+
+    doc.build(Story)
+
+    fs = FileSystemStorage("/tmp")
+    with fs.open("carta.pdf") as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="carta.pdf"'
+    return response
+
 def gerar_pdf_oficio(oficio):
 
     doc = SimpleDocTemplate("/tmp/oficio.pdf")
